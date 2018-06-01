2f08d1fa334b8170761a5cb9265bfd649d282b71
Justin Ho <justinho@Justins-MacBook-Pro.local>
2018-06-01 12:33:31 +0800

    added read url file middleware due to using wordpress

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 const asyncBusboy = require('async-busboy');
+const axios = require('axios');
 
 const validateRequest = require('../lib/validateRequest');
 
@@ -18,6 +19,17 @@ class Middleware {
       ctx.status = 400;
       ctx.body = { code: 400, data: 'Bad request' };
     }
+  }
+
+  static async urlFileReader(ctx, next) {
+    const {
+      file1url,
+      file2url,
+    } = ctx.request.body;
+    const file1 = await axios.get(file1url);
+    const file2 = await axios.get(file2url);
+    ctx.state.files = [file1, file2];
+    await next();
   }
 }
 
